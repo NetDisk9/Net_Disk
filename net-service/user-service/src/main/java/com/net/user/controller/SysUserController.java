7dab6ad7e3e92bc4f25d0d2f3b7b1113ade1d039
倪圳褒 <1121507128@qq.com>
2024-03-22 21:39:34 +0800

    添加功能

--- net-service/user-service/src/main/java/com/net/user/controller/SysUserController.java
+++ net-service/user-service/src/main/java/com/net/user/controller/SysUserController.java
@@ -1,9 +1,14 @@
 package com.net.user.controller;
 
 
+import cn.hutool.core.bean.BeanUtil;
+import com.alibaba.nacos.common.utils.MD5Utils;
+import com.alibaba.spring.util.BeanUtils;
 import com.baomidou.mybatisplus.core.conditions.Wrapper;
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.core.conditions.update.LambdaUpdateWrapper;
+import com.baomidou.mybatisplus.core.toolkit.Wrappers;
 import com.net.common.context.BaseContext;
 import com.net.common.dto.ResponseResult;
 import com.net.common.enums.ResultCodeEnum;
@@ -12,9 +17,14 @@ import com.net.user.pojo.dto.LoginDTO;
 import com.net.user.pojo.dto.RegisterDTO;
 import com.net.user.pojo.dto.UpdatePasswordDTO;
 import com.net.user.pojo.dto.UserDTO;
+import com.net.user.pojo.vo.UserVO;
 import com.net.user.service.SysUserService;
 import lombok.RequiredArgsConstructor;
+import org.bouncycastle.crypto.digests.SHA256Digest;
+import org.bouncycastle.jcajce.provider.digest.SHA256;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 
 /**
@@ -39,17 +49,31 @@ public class SysUserController {
     public ResponseResult register(@RequestBody RegisterDTO registerDTO){
         return ResponseResult.okResult(ResultCodeEnum.SUCCESS);
     }
-    @PutMapping("/update/password")
-    public ResponseResult updatePassword(@RequestBody UpdatePasswordDTO updatePasswordDTO){
-        return ResponseResult.okResult(ResultCodeEnum.SUCCESS);
+    @GetMapping("/info")
+    public ResponseResult getUserInfo(){
+        LambdaQueryWrapper<SysUser> queryWrapper =new LambdaQueryWrapper<>();
+        queryWrapper.eq(SysUser::getId,1);
+        SysUser one = userService.getOne(queryWrapper);
+        UserVO userVO = BeanUtil.copyProperties(one, UserVO.class);
+        return ResponseResult.okResult(userVO);
     }
     @PutMapping("/update")
     public ResponseResult update(@RequestBody UserDTO userDTO){
-        LambdaUpdateWrapper<SysUser> wrapper = new LambdaUpdateWrapper<>();
-//        wrapper.eq(SysUser::getId, BaseContext.getCurrentId());
-        wrapper.eq(SysUser::getId, 1);
-        wrapper.set(SysUser::getUsername, userDTO.getUsername());
-        userService.update(wrapper);
+        LambdaUpdateWrapper<SysUser>updateWrapper =new LambdaUpdateWrapper<>();
+        updateWrapper.eq(SysUser::getId, 1).
+                set(SysUser::getUsername, userDTO.getUsername());
+        userService.update(updateWrapper);
+        return ResponseResult.okResult(ResultCodeEnum.SUCCESS);
+    }
+    @PutMapping("/update/password")
+    public ResponseResult updatePassword(@RequestBody UpdatePasswordDTO updatePasswordDTO){
+        if (!updatePasswordDTO.getOldPassword().equals(updatePasswordDTO.getNewPassword())) {
+            return ResponseResult.errorResult(ResultCodeEnum.LOGIN_PASSWORD_ERROR);
+        }
+        return ResponseResult.okResult(ResultCodeEnum.SUCCESS);
+    }
+    @PostMapping("/avatar")
+    public ResponseResult uploadAvatar(@RequestBody MultipartFile multipartFile){
         return ResponseResult.okResult(ResultCodeEnum.SUCCESS);
     }
 
