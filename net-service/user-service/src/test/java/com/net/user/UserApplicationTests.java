c84c662e99ac434073c15e152247b3f7df46ac86
rua <872919781@qq.com>
2024-03-25 14:21:26 +0800

    a

--- net-service/user-service/src/test/java/com/net/user/UserApplicationTests.java
+++ net-service/user-service/src/test/java/com/net/user/UserApplicationTests.java
@@ -12,31 +12,31 @@ import javax.annotation.Resource;
 
 //@SpringBootTest
 public class UserApplicationTests {
-    @Resource
-    SysUserService userService;
-    @Resource
-    RedisUtil redisUtil;
-
-    @Test
-    public void redisTest(){
-        redisUtil.set("abc",123);
-        System.out.println(redisUtil.get("abc"));
-    }
-    @Test
-    public void add() {
-        userService.save(
-                SysUser.builder().sex(0).
-                email("11215017128@qq.com").avatar("null").password("12345").
-                username("baozi").status(0).nickname("包子").build()
-        );
-    }
-    @Test
-    public void testPassword(){
-        System.out.println(SHAUtil.encrypt("Baozi0318"));
-    }
-    @Test
-    public void test(){
-        System.out.println(RegexUtil.checkPasswordValid("consequat"));
-    }
+//    @Resource
+//    SysUserService userService;
+//    @Resource
+//    RedisUtil redisUtil;
+//
+//    @Test
+//    public void redisTest(){
+//        redisUtil.set("abc",123);
+//        System.out.println(redisUtil.get("abc"));
+//    }
+//    @Test
+//    public void add() {
+//        userService.save(
+//                SysUser.builder().sex(0).
+//                email("11215017128@qq.com").avatar("null").password("12345").
+//                username("baozi").status(0).nickname("包子").build()
+//        );
+//    }
+//    @Test
+//    public void testPassword(){
+//        System.out.println(SHAUtil.encrypt("Baozi0318"));
+//    }
+//    @Test
+//    public void test(){
+//        System.out.println(RegexUtil.checkPasswordValid("consequat"));
+//    }
 
 }
